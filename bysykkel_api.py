fb289b17a63aaa5f94bc62b2813ac5755b13a731
ivholmlu <ivholmlu@nmbu.no>
2024-12-03 15:02:58 +0100

    Current shitty state, but mapbox is kinda working

--- bysykkel_api.py
+++ bysykkel_api.py
@@ -1,8 +1,13 @@
 import requests
 
-
+#Links
 _stations_link = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
+_availability_link = "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
 
 def get_all_stations(headers) -> dict:
     response = requests.get(_stations_link, headers=headers)
+    return response.json()
+
+def get_bike_info(headers) -> dict:
+    response = requests.get(_availability_link, headers=headers)
     return response.json()
